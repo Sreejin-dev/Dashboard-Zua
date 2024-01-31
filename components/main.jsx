10b5d0b360c859c13ddf76550b96d92e112d7909
Sreejin-dev <sreejincp84@gmail.com>
2024-01-31 15:53:30 +0530

    modified:   components/main.jsx

--- components/main.jsx
+++ components/main.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 const main = () => {
   return (
     <div className="text-white flex justify-center items-center min-h-svh ">
-      <div className="gap-8 flex flex-col items-center">
+      <div className="gap-8 flex flex-col items-center w-full px-10">
         <div className="flex justify-center">
           <Image
             placeholder="blur"
@@ -17,11 +17,11 @@ const main = () => {
         <div className="flex justify-center text-[#28EFAC] font-bold text-6xl">
           <h1>Zua</h1>{" "}
         </div>
-        <div className="font-bold gap-5 flex md:text-5xl">
-          <button className="text-[#24C8B0]  border-[#2A4A3D] border-4 p-2 px-10 rounded-3xl hover:border-[#0AF2A9] hover:text-[45px] transition-all">
+        <div className="font-bold gap-5 flex md:text-3xl max-md:flex-col w-full justify-center">
+          <button className="text-[#24C8B0]  border-[#2A4A3D] border-4 p-2 px-10 rounded-3xl hover:border-[#0AF2A9] hover:text-[25px] transition-all">
             Invite Me
           </button>
-          <button className="border rounded-full p-2 px-10 bg-gradient-to-r from-[#0AF2A9] to-[#077ACB] hover:border-blue-800 hover:text-[45px]  transition-all">
+          <button className="border rounded-full p-2 px-10 bg-gradient-to-r from-[#0AF2A9] to-[#077ACB] hover:border-blue-800 hover:text-[25px]  transition-all">
             Dashboard
           </button>
         </div>
