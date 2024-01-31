d53477cba3fb018dbf4db4672f659befb4bbf6c6
Sreejin-dev <sreejincp84@gmail.com>
2024-01-31 11:34:41 +0530

    team section fixed 	new file:   app/Team/page.jsx 	modified:   app/globals.css 	new file:   assets/person-profile-image-icon.png 	new file:   components/Team/Team.jsx 	modified:   components/header.jsx 	modified:   package-lock.json 	modified:   package.json

---
+++ app/Team/page.jsx
@@ -0,0 +1,14 @@
+
+import Team from '@/components/Team/Team'
+import { Calistoga } from 'next/font/google'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='flex h-svh justify-center items-center'>
+    <Team/>
+    </div>
+  )
+}
+
+export default page
---
+++ components/Team/Team.jsx
@@ -0,0 +1,52 @@
+import Image from "next/image";
+import React from "react";
+import person from "@/assets/zoulogo.jpeg";
+
+function TeamMember({ name, role }) {
+  return (
+    <div className="p-4">
+      <div className="shadow-md bg-gray-800 text-white rounded-md overflow-hidden p-10 hover:p-5 transition-all  hover:bg-[#DC143C] duration-500">
+        <Image
+          src={person}
+          alt={name}
+          className="w-full border-4 rounded-full border-red-600 hover:border-white transition-all filter grayscale hover:grayscale-0"
+        />
+        <div className="p-4 text-center">
+          <h2 className="text-xl font-bold">{name}</h2>
+          <p className="text-gray-600  text-gray-200">{role}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Team() {
+  const team = [
+    { name: "SREEJIN CP", role: "WEB DEVELOPER & DESIGNER" },
+    { name: "CRUZ GAMING", role: "BOT DEVELOPER & SAMP DEV" },
+    { name: "RJR YT", role: "SAMP DEV & DC DESIGNER" },
+    { name: "A.GFX", role: "DEVELOPER & CREATOR" },
+  ];
+
+  return (
+    <div>
+      <div className="text-center">
+        {" "}
+        <h2 className="text-green-500 text-5xl font-bold">Our Team</h2>
+        <p className="text-green-200 text-lg font-bold">
+          about our team members
+        </p>
+      </div>
+
+      <br />
+
+      <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
+        {team.map((member, index) => (
+          <TeamMember key={index} name={member.name} role={member.role} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Team;
