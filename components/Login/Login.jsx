274706ae09f70dabcad06a760ab5db0441e931c9
Sreejin-dev <sreejincp84@gmail.com>
2024-02-02 17:25:15 +0530

    modified:   components/Login/Login.jsx

--- components/Login/Login.jsx
+++ components/Login/Login.jsx
@@ -5,25 +5,25 @@ import Image from "next/image";
 const Login = () => {
   return (
     <div className="text-white flex justify-center ">
-       <a href="/" >
-          {" "}
-          <button className=" bg-gradient-to-r from-[#0AF2A9] to-[#077ACB]  absolute left-3 top-3 hover:bg-emerald-600 transition-all duration-500 text-white border-2 p-2 font-mono font-bold rounded-3xl">
-
-            Back
-          </button>
-
-
-
-        </a>
+      <a href="/">
+        {" "}
+        <button className=" bg-gradient-to-r from-[#0AF2A9] to-[#077ACB]  absolute left-3 top-3 hover:bg-emerald-600 transition-all duration-500 text-white border-2 p-2 font-mono font-bold rounded-3xl">
+          Back
+        </button>
+      </a>
       {/* zou bot */}
       <div className="md:flex text-center gap-32 items-center pt-20">
-       
-
         <div>
-          <Image className="rounded-full" priority width={300} alt="zou" src={zou} />
+          <Image
+            className="rounded-full"
+            priority
+            width={300}
+            alt="zou"
+            src={zou}
+          />
         </div>
         <div>
-          <h1 className="text-6xl font-bold pt-5 font-sans">
+          <h1 className="text-6xl font-bold pt-5  font-sans">
             Zou-Bot <br /> Dashboard
           </h1>
           <p className="pb-5">
@@ -34,7 +34,7 @@ const Login = () => {
           <a
             href="https://discord.com/oauth2/authorize?client_id=855283499526782996&permissions=704374636287&scope=bot%20applications.commands
 "
-            className="border font-semibold  text-lg bg-transparent rounded-3xl  p-2 px-16 hover:text-blue-700 hover:bg-white duration-500"
+            className="border font-semibold  text-lg bg-transparent  rounded-3xl md:p-2 p-3 px-16 hover:text-blue-700 hover:bg-white duration-500"
           >
             <button>Log in With Discord</button>
           </a>
