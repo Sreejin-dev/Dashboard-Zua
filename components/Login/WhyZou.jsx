934014978faaf83b0e6fada02ac6cb34a32ff4fa
muhammed-farooq <faroooq9207@gmail.com>
2024-02-16 14:07:22 +0530

    modified:   components/Login/WhyZou.jsx

--- components/Login/WhyZou.jsx
+++ components/Login/WhyZou.jsx
@@ -4,36 +4,45 @@ const WhyZou = () => {
   return (
     <div className="text-white">
       <div>
-        <h1 className="flex justify-center text-blue-400 text-3xl  p-10">
+        <h1 className="flex justify-center text-blue-400 text-4xl font-bold  border-b-2  p-10">
           Why Zou-bot?
         </h1>
-        <div className="flex flex-col items-center ">
-        <h1 className="before:border-teal-400 before:border before:mr-5 after:ml-5 after:border-teal-400 after:border">Fast Respond</h1>
-          <div className="flex gap-20 pt-10 ">
-           
-          
-            <ul className="marker:text-green list-outside list-disc ml-6 ">
+        <div className="flex flex-col px-52  pt-20">
+          <h1 className="before:border-teal-400 before:border before:mr-5 after:ml-5 after:border-teal-400 after:border text-2xl font-mono font-bold">
+            Fast Respond
+          </h1>
+          <div className="flex flex-col gap-20 pt-10 ">
+            <ul className="marker:text-green list-outside list-disc ml-6 text-xl font-semibold font-thin pl-10 ">
               <li>the fastest responding bot</li>
               <li>the fastest responding bot</li>
               <li>the fastest responding bot</li>
               <li>the fastest responding bot</li>
               <li>the fastest responding bot</li>
             </ul>
-            <ul className="marker:text-green list-outside list-disc ml-6 ">
+            <h1 className="before:border-teal-400 before:border before:mr-5 after:ml-5 after:border-teal-400 after:border  text-2xl font-mono font-bold">
+              Fast Respond
+            </h1>
+            <ul className="marker:text-green list-outside list-disc ml-6 text-xl font-semibold font-thin pl-10 ">
               <li>the fastest responding bot</li>
               <li>the fastest responding bot</li>
               <li>the fastest responding bot</li>
               <li>the fastest responding bot</li>
               <li>the fastest responding bot</li>
             </ul>
-            <ul className="marker:text-green list-outside list-disc ml-6 ">
+            <h1 className="before:border-teal-400 before:border before:mr-5 after:ml-5 after:border-teal-400 after:border  text-2xl font-mono font-bold">
+              Fast Respond
+            </h1>
+            <ul className="marker:text-green list-outside list-disc ml-6 text-xl font-semibold font-thin pl-10 ">
               <li>the fastest responding bot</li>
               <li>the fastest responding bot</li>
               <li>the fastest responding bot</li>
               <li>the fastest responding bot</li>
               <li>the fastest responding bot</li>
             </ul>
-            <ul className="marker:text-green list-outside list-disc ml-6 ">
+            <h1 className="before:border-teal-400 before:border before:mr-5 after:ml-5 after:border-teal-400 after:border  text-2xl font-mono font-bold">
+              Fast Respond
+            </h1>
+            <ul className="marker:text-green list-outside list-disc ml-6 text-xl font-semibold font-thin pl-10 ">
               <li>the fastest responding bot</li>
               <li>the fastest responding bot</li>
               <li>the fastest responding bot</li>
