f8414d467a17c78ff160b5de45df0481fb7c201a
muhammed-farooq <faroooq9207@gmail.com>
2024-02-16 13:55:13 +0530

    modified:   components/Login/WhyZou.jsx

--- components/Login/WhyZou.jsx
+++ components/Login/WhyZou.jsx
@@ -3,13 +3,45 @@ import React from "react";
 const WhyZou = () => {
   return (
     <div className="text-white">
-      <div  >
-       
-        <h1 className="flex justify-center">Why Zou-bot?</h1>
+      <div>
+        <h1 className="flex justify-center text-blue-400 text-3xl  p-10">
+          Why Zou-bot?
+        </h1>
         <div className="flex flex-col items-center ">
-            <div><h1>Reaction</h1>
-            <ul className="marker:text-green list-outside list-disc ml-6"><li>first</li></ul></div>
-            <div></div>
+        <h1 className="before:border-teal-400 before:border before:mr-5 after:ml-5 after:border-teal-400 after:border">Fast Respond</h1>
+          <div className="flex gap-20 pt-10 ">
+           
+          
+            <ul className="marker:text-green list-outside list-disc ml-6 ">
+              <li>the fastest responding bot</li>
+              <li>the fastest responding bot</li>
+              <li>the fastest responding bot</li>
+              <li>the fastest responding bot</li>
+              <li>the fastest responding bot</li>
+            </ul>
+            <ul className="marker:text-green list-outside list-disc ml-6 ">
+              <li>the fastest responding bot</li>
+              <li>the fastest responding bot</li>
+              <li>the fastest responding bot</li>
+              <li>the fastest responding bot</li>
+              <li>the fastest responding bot</li>
+            </ul>
+            <ul className="marker:text-green list-outside list-disc ml-6 ">
+              <li>the fastest responding bot</li>
+              <li>the fastest responding bot</li>
+              <li>the fastest responding bot</li>
+              <li>the fastest responding bot</li>
+              <li>the fastest responding bot</li>
+            </ul>
+            <ul className="marker:text-green list-outside list-disc ml-6 ">
+              <li>the fastest responding bot</li>
+              <li>the fastest responding bot</li>
+              <li>the fastest responding bot</li>
+              <li>the fastest responding bot</li>
+              <li>the fastest responding bot</li>
+            </ul>
+          </div>
+          <div></div>
         </div>
       </div>
     </div>
