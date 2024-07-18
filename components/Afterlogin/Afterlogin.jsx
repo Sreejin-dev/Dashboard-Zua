48e455d7435007c5c166832eba9f4afa6493e36d
Sreejin-dev <sreejincp84@gmail.com>
2024-07-18 13:44:36 +0530

    modified:   app/globals.css 	modified:   components/Afterlogin/Afterlogin.jsx

--- components/Afterlogin/Afterlogin.jsx
+++ components/Afterlogin/Afterlogin.jsx
@@ -3,47 +3,51 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-
 import { Navigation } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
-import imagenew from "@/assets/zoulogo.jpeg"
+import imagenew from "@/assets/zoulogo.jpeg";
+
 const Afterlogin = () => {
-  const boxesData = [
-    {
-      id: [1,2,3],
-      name: "gild 1",
-      link: "box1-link",
-      imageSrc:
-        imagenew,
-    },
-  ];
+  const boxData = {
+    name: "Guild",
+    link: "box1-link",
+    imageSrc: imagenew,
+  };
+
+  const boxesData = Array(18)
+    .fill()
+    .map((_, index) => ({
+      ...boxData,
+      id: index + 1,
+    }));
 
   return (
     <>
-    <h1 className="text-5xl text-green-500">Under maintenance 😁</h1>
-      <Swiper navigation={true} modules={[Navigation]} className="mySwiper  ">
-        <SwiperSlide>
-          <div className="grid lg:grid-cols-3 pt-10 px-5 gap-10 place-items-center bg-gradient-to-l from-black to-gray-500 ">
+      <Swiper navigation={true} modules={[Navigation]} className="mySwiper ">
+        <SwiperSlide >
+          <div className="grid lg:grid-cols-3 pt-10 px-5 gap-10 place-items-center bg-gradient-to-l from-black to-green-900">
             {boxesData.map((box) => (
               <div key={box.id}>
-                <div className="bg-white rounded-lg w-[300px] h-[100px] flex text-black font-sans ">
-                  <div className="w-full flex items-center justify-center">
+                <div className="bg-black  shadow-inner shadow-slate-500 p-2 rounded-lg w-[300px] h-[100px] flex text-black font-sans">
+                  <div className="w-full  flex items-center justify-center ">
                     <Image
                       src={box.imageSrc}
                       alt={box.name}
                       height={500}
                       width={500}
-                      className="rounded-full object-fill  w-[60%] h-[90%]"
+                      className="rounded-full object-fill w-[60%] h-[100%]"
                     />
                   </div>
-                  <div className="w-full flex flex-col justify-center gap-3 items-center">
-                    <h1 className="text-2xl font-bold">{box.name}</h1>
+                  <div className="w-full flex flex-col justify-center items-center gap-3">
+                    <h1 className=" text-2xl font-bold text-white  overflow-hidden  max-w-32" >
+                      {box.name}{box.id}
+                    </h1>
                     <Link
                       href={box.link}
-                      className="px-5 rounded-md text-2xl font-semibold  bg-cyan-400 flex"
+                      className="px-5 py-[3px] rounded-md text-xl hover:opacity-65 duration-200 font-semibold bg-cyan-400 flex"
                     >
-                      invite
+                      INVITE
                     </Link>
                   </div>
                 </div>
@@ -51,8 +55,9 @@ const Afterlogin = () => {
             ))}
           </div>
         </SwiperSlide>
-
-        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>
+          <h1 className="text-white">swiper slider</h1>
+        </SwiperSlide>
       </Swiper>
     </>
   );
